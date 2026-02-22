3c5ee80995e3951b0306ce73115d9219b23f1baa
ReyyArchadiaa <ryyxiaoyan@gmail.com>
2026-02-22 12:04:00 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,8 @@
 <?php
-define('PUSHER_APP_ID', trim(getenv('PUSHER_APP_ID')));
-define('PUSHER_KEY', trim(getenv('PUSHER_KEY')));
-define('PUSHER_SECRET', trim(getenv('PUSHER_SECRET')));
-define('PUSHER_CLUSTER', trim(getenv('PUSHER_CLUSTER')));
+
+define("PUSHER_APP_ID", getenv("PUSHER_APP_ID"));
+define("PUSHER_KEY", getenv("PUSHER_KEY"));
+define("PUSHER_SECRET", getenv("PUSHER_SECRET"));
+define("PUSHER_CLUSTER", getenv("PUSHER_CLUSTER"));
+
 ?>
