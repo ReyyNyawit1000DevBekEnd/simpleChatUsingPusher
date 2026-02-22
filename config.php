99da9ed0865e8f574ca9f8c9cd7357822bc7ce2b
ReyyArchadiaa <ryyxiaoyan@gmail.com>
2026-02-22 09:50:43 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
+?>
 define('PUSHER_APP_ID', getenv('PUSHER_APP_ID'));
 define('PUSHER_KEY', getenv('PUSHER_KEY'));
 define('PUSHER_SECRET', getenv('PUSHER_SECRET'));
