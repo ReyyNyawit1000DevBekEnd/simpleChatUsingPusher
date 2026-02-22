e0583323071f743eb5a4ed36ec4ce4a398764c08
ReyyArchadiaa <ryyxiaoyan@gmail.com>
2026-02-22 09:47:14 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,11 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "chat_demo");
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$pass = getenv("MYSQLPASSWORD");
+$db   = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT");
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
