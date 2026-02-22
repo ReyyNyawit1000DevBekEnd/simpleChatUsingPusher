f4e46feb0fc6cbbe58449b7c6e951c3a64fdd76b
ReyyArchadiaa <ryyxiaoyan@gmail.com>
2026-02-22 12:06:38 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,23 @@
 <?php
 
-define("PUSHER_APP_ID", getenv("PUSHER_APP_ID"));
-define("PUSHER_KEY", getenv("PUSHER_KEY"));
-define("PUSHER_SECRET", getenv("PUSHER_SECRET"));
-define("PUSHER_CLUSTER", getenv("PUSHER_CLUSTER"));
+$db_url = getenv("MYSQL_URL");
+
+if(!$db_url){
+    die("DB URL NOT FOUND");
+}
+
+$parts = parse_url($db_url);
+
+$host = $parts["host"];
+$user = $parts["user"];
+$pass = $parts["pass"];
+$dbname = ltrim($parts["path"],"/");
+$port = $parts["port"] ?? 3306;
+
+$conn = new mysqli($host,$user,$pass,$dbname,$port);
+
+if($conn->connect_error){
+    die("DB ERROR : ".$conn->connect_error);
+}
 
 ?>
