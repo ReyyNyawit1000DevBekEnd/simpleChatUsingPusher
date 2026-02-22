140a371deeb06bd58b15afc7e8babd600e0707e8
ReyyArchadiaa <ryyxiaoyan@gmail.com>
2026-02-22 09:44:54 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,9 @@
 <?php
-// enable error
-//composer require pusher/pusher-php-server
-
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-define('PUSHER_APP_ID', 'xxxxx');
-define('PUSHER_KEY', 'xxxxxx');
-define('PUSHER_SECRET', 'xxxxx');
-define('PUSHER_CLUSTER', 'xxxxx');
+define('PUSHER_APP_ID', getenv('PUSHER_APP_ID'));
+define('PUSHER_KEY', getenv('PUSHER_KEY'));
+define('PUSHER_SECRET', getenv('PUSHER_SECRET'));
+define('PUSHER_CLUSTER', getenv('PUSHER_CLUSTER'));
 ?>
